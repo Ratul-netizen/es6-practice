6830f736349f2954e8ee11c07e188fe11269ff24
Ratul-netizen <Ratul-netizen@github.com>
2021-02-01 11:57:37 +0600

    First practice

---
+++ letConst.js
@@ -0,0 +1,12 @@
+const hubby = "Elias Kanchon"; 
+//console.log(hubby);
+
+const numbers = [12, 45];
+numbers [1] = 88;
+numbers.push(12);
+const nayok = {name:"Shakib Khan", phone:4578};
+//console.log(numbers);
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
